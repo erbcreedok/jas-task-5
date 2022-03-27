d38f28eb43f1fe62e0c467a4ff4741f49df68047
Yerbol Syzdyk <erbolsyzdyq@gmail.com>
2022-03-27 11:44:15 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import {Home} from "./pages/Home";
 import {About} from "./pages/About";
+import {useState} from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0)
 
     return (
         <div className="App">
+            <nav className="nav">
+                <Link to="/">Fibonacci</Link>
+                <Link to="/about">Factorial</Link>
+            </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
