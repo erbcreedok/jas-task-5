aa4361d9ffc339dbed841e9fd6e592d3572ed252
Yerbol Syzdyk <erbolsyzdyq@gmail.com>
2022-03-30 12:25:44 +0600

    add sorting and pagination

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,32 +1,43 @@
 import {useEffect, useState} from "react";
-import {Button, Container, Grid, TextField} from "@mui/material";
+import {
+    Button,
+    Container,
+    FormControl,
+    Grid,
+    InputLabel,
+    MenuItem,
+    Pagination,
+    Select,
+    TextField
+} from "@mui/material";
 import {MovieItem} from "../components/MovieItem";
 
 export function MoviesPage() {
+    const [pageInfo, setPageInfo] = useState({
+        page: 1,
+        total_pages: 0,
+    })
     const [movies, setMovies] = useState([])
     const [query, setQuery] = useState('')
+    const [sortBy, setSortBy] = useState('popularity.desc')
 
     useEffect(() => {
-        loadDefaultMovies()
+        searchMovies()
     }, [])
 
-    function loadDefaultMovies() {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=d65708ab6862fb68c7b1f70252b5d91c&language=ru-RU&sort_by=popularity.desc&include_adult=false&include_video=true&page=3&with_watch_monetization_types=flatrate`)
-            .then((res) => res.json())
-            .then((data) => {
-                setMovies(data.results)
-            })
-    }
-
-    function searchMovies() {
-        if (!query || query.length < 1) {
-            loadDefaultMovies()
-            return
+    function searchMovies({ page = 1, sort = sortBy } = {}) {
+        let method = 'discover'
+        if (query && query.length > 0) {
+            method = 'search'
         }
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=d65708ab6862fb68c7b1f70252b5d91c&language=ru-RU&page=2&include_adult=false&query=${query}`)
+        fetch(`https://api.themoviedb.org/3/${method}/movie?sort_by=${sort}&api_key=d65708ab6862fb68c7b1f70252b5d91c&language=ru-RU&include_adult=false&include_video=true&page=${page}&with_watch_monetization_types=flatrate&query=${query}`)
             .then((res) => res.json())
             .then((data) => {
                 setMovies(data.results)
+                setPageInfo({
+                    page: data.page,
+                    total_pages: Math.min(data.total_pages, 500)
+                })
             })
     }
 
@@ -34,9 +45,30 @@ export function MoviesPage() {
         <Container maxWidth="xl">
             <div style={{ display: 'flex', alignItems: 'center' }}>
                 <h1>Movies</h1>
-                <div style={{ marginLeft: 'auto' }}>
+                <div style={{ marginLeft: 'auto', flexGrow: 1, maxWidth: '300px' }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Sort By</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={sortBy}
+                            label="Sort by"
+                            disabled={query && query.length > 0}
+                            onChange={(e) => {
+                                setSortBy(e.target.value);
+                                searchMovies({ sort: e.target.value })
+                            }}
+                            size="small"
+                        >
+                            <MenuItem value="popularity.desc">Popularity</MenuItem>
+                            <MenuItem value="release_date.desc">Release Date</MenuItem>
+                            <MenuItem value="vote_average.desc">Rating</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+                <div style={{ marginLeft: '5px' }}>
                     <TextField value={query} onChange={(e) => setQuery(e.target.value)} size="small" label="Search" />
-                    <Button onClick={searchMovies}>Search</Button>
+                    <Button onClick={() => searchMovies()}>Search</Button>
                 </div>
             </div>
             <Grid container spacing={2}>
@@ -46,6 +78,8 @@ export function MoviesPage() {
                     </Grid>
                 ))}
             </Grid>
+
+            <Pagination count={pageInfo.total_pages} page={pageInfo.page} onChange={(e, value) => searchMovies({ page: value })}/>
         </Container>
     )
 }
